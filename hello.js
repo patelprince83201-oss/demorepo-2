67f428b2a530a237894b1608cec8eed8613751f0
patel-prince1 <patelprince83201@gamil.com>
2026-01-22 18:23:21 +0530

    add a fectorial num

---
+++ hello.js
@@ -0,0 +1,10 @@
+
+function factoeial() {
+    let facto = 1;
+    let num = 3
+    for(let i=1; i<=num; i++){
+        facto = facto * i;
+    }
+console.log(facto);
+alert(`factorial of ${num} is : ${facto}`)
+}
